f77607afafeec9e8d26a2b92cd6ed729ee9733fb
mitchelst <mitchel@bendingtherules.nl>
2021-06-01 21:13:35 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
+// Details
+// Shipping
+// Payment
+
+type CheckoutStep = "Details" | "Shipping" | "Payment";
+
 export default function App() {
-  return <></>;
+  const [checkoutStep, setCheckoutStep] = useState<CheckoutStep>("Details");
+
+  return (
+    <>
+      {checkoutStep === "Details" && (
+        <>
+          <h1>Details</h1>
+          <button type="button" onClick={() => setCheckoutStep("Shipping")}>
+            Next
+          </button>
+        </>
+      )}
+
+      {checkoutStep === "Shipping" && (
+        <>
+          <h1>Shipping</h1>
+          <button type="button" onClick={() => setCheckoutStep("Payment")}>
+            Next
+          </button>
+        </>
+      )}
+
+      {checkoutStep === "Payment" && (
+        <>
+          <h1>Payment</h1>
+        </>
+      )}
+    </>
+  );
 }
