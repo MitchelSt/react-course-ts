0bac28753b18948d1f6aa9d7d45cc5821d452d31
mitchelst <mitchel@bendingtherules.nl>
2021-06-01 22:57:04 +0200

    Revert "wip"
    This reverts commit 7cc632921a666cd9d25d754196f7dc1da3e1cff9.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,3 @@
-import DataGrid from "./components/DataGrid";
-
 export default function App() {
-  const users = [
-    { id: 1, name: "John", age: 55 },
-    { id: 2, name: "Mitchel", age: 23 },
-    { id: 3, name: "Mike", age: 50 },
-  ];
-
-  return (
-    <>
-      <DataGrid items={users} />
-    </>
-  );
+  return <></>;
 }
