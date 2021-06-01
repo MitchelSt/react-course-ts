b206dbad94c784e55f3c4cbcdfd661112d391796
mitchelst <mitchel@bendingtherules.nl>
2021-06-01 11:52:24 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,51 @@
+import { useState } from "react";
+
+const defaultFormData = {
+  title: "",
+  body: "",
+};
+
 export default function App() {
-  return <></>;
+  const [formData, setFormData] = useState(defaultFormData);
+  const { title, body } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    setFormData(defaultFormData);
+  };
+
+  return (
+    <>
+      <h1>Form</h1>
+      <p>Create a post</p>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="title">Title</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => onChange(e)}
+        />
+        <br />
+        <br />
+        <label htmlFor="body">Body</label>
+        <br />
+        <input type="text" id="body" value={body} onChange={onChange} />
+        <br />
+        <br />
+        <button type="submit">Upload post</button>
+      </form>
+    </>
+  );
 }
