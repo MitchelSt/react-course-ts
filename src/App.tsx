64a3f4aed4a0163570597bc41519da9648478789
mitchelst <mitchel.stroosnijder@gmail.com>
2021-09-28 21:14:56 +0200

    add: starter code

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import Component from "./components/Component";
+
 export default function App() {
-  return <></>;
+  const items = [
+    {
+      id: 1,
+      title: "A nice sunset",
+      imageUrl:
+        "https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80",
+    },
+    {
+      id: 2,
+      quote:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, quam ea. Nisi nulla earum itaque, sapiente exercitationem, laudantium sunt fuga dolores repellendus, expedita dicta. Voluptates minima laboriosam odit reprehenderit magnam!",
+    },
+  ];
+
+  return <Component items={items} />;
 }
---
+++ src/components/Component.tsx
@@ -0,0 +1,31 @@
+export interface Item {
+  id: number;
+  title?: string;
+  imageUrl?: string;
+  quote?: string;
+}
+
+export interface ComponentProps {
+  items: Item[];
+}
+
+export default function Component({ items }: ComponentProps) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          {item.title && <p>{item.title}</p>}
+          {item.imageUrl && (
+            <img
+              style={{ maxWidth: "15rem" }}
+              src={item.imageUrl}
+              alt={item.title}
+            />
+          )}
+
+          {item.quote && <p>{item.quote}</p>}
+        </li>
+      ))}
+    </ul>
+  );
+}
