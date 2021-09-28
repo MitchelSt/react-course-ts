a9d99986f140b2567cd4b0e1857b497a661c9d47
mitchelst <mitchel.stroosnijder@gmail.com>
2021-09-28 21:31:58 +0200

    change: export and mapping

--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -1,31 +1,33 @@
-export interface Item {
+interface Item {
   id: number;
   title?: string;
   imageUrl?: string;
   quote?: string;
 }
 
-export interface ComponentProps {
+interface ComponentProps {
   items: Item[];
 }
 
 export default function Component({ items }: ComponentProps) {
   return (
     <ul>
-      {items.map((item) => (
-        <li key={item.id}>
-          {item.title && <p>{item.title}</p>}
-          {item.imageUrl && (
-            <img
-              style={{ maxWidth: "15rem" }}
-              src={item.imageUrl}
-              alt={item.title}
-            />
-          )}
+      {items.map((item) => {
+        return (
+          <li key={item.id}>
+            {item.title && <p>{item.title}</p>}
+            {item.imageUrl && (
+              <img
+                style={{ maxWidth: "15rem" }}
+                src={item.imageUrl}
+                alt={item.title}
+              />
+            )}
 
-          {item.quote && <p style={{ fontStyle: "italic" }}>{item.quote}</p>}
-        </li>
-      ))}
+            {item.quote && <p style={{ fontStyle: "italic" }}>{item.quote}</p>}
+          </li>
+        );
+      })}
     </ul>
   );
 }
