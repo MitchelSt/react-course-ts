997e698b1e2214f6d6fba4cc85d4b29cccec379f
mitchelst <mitchel.stroosnijder@gmail.com>
2021-10-02 15:49:19 +0200

    add: finished code

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ export default function App() {
   const items: Item[] = [
     {
       id: 1,
+      type: "imageItem",
       title: "A nice sunset",
       imageUrl:
         "https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80",
     },
     {
       id: 2,
+      type: "quoteItem",
       quote:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, quam ea. Nisi nulla earum itaque, sapiente exercitationem, laudantium sunt fuga dolores repellendus, expedita dicta. Voluptates minima laboriosam odit reprehenderit magnam!",
     },
